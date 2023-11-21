b0a466eaca3cb4cf0eff28d531d864693cf9d5e6
TyuiuFaizullinDR@outlook.com <toxen2005@mail.ru>
2023-11-22 02:33:59 +0500

    Task7

--- Tyuiu.FaizullinDR.Sprint5.Task7.V1.Test/DataServiceTest.cs
+++ Tyuiu.FaizullinDR.Sprint5.Task7.V1.Test/DataServiceTest.cs
@@ -11,6 +11,20 @@ namespace Tyuiu.FaizullinDR.Sprint5.Task7.V1.Test
         [TestMethod]
         public void TestMethod1()
         {
+            string path = @"C:\DataSprint5\InPutDataFileTask7V1.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool FileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, FileExists);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string path = @"C:\Users\toxen\source\repos\Tyuiu.FaizullinDR.Sprint5\Tyuiu.FaizullinDR.Sprint5.Task7.V1\bin\debug\OutPutDataFileTask7V1.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool FileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, FileExists);
         }
     }
 }
